6a7e97dbf402ff0f94efec6038ba6a4abda71d22
Marcille Hu <marcille.hu@gmail.com>
2025-07-23 23:22:23 +0800

    feat: Update to new chatbot API

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,7 +1,8 @@
 use miniserve::{http, Content, Request, Response};
 use serde::{Deserialize, Serialize};
-use std::sync::Arc;
+use std::sync::{Arc, LazyLock};
 use tokio::join;
+use tokio::sync::{mpsc, oneshot};
 
 async fn index(_req: Request) -> Response {
     let content = include_str!("../index.html").to_string();
@@ -18,6 +19,26 @@ struct ChatResponse {
     messages: Vec<String>,
 }
 
+async fn query_chat(messages: &Arc<Vec<String>>) -> Vec<String> {
+    type Payload = (Arc<Vec<String>>, oneshot::Sender<Vec<String>>);
+
+    static SENDER: LazyLock<mpsc::Sender<Payload>> = LazyLock::new(|| {
+        let (tx, mut rx) = mpsc::channel::<Payload>(1024);
+        tokio::spawn(async move {
+            let mut chatbot = chatbot::Chatbot::new(vec![":-)".to_string(), "^^".to_string()]);
+            while let Some((messages, responder)) = rx.recv().await {
+                let response = chatbot.query_chat(&messages).await;
+                responder.send(response).unwrap();
+            }
+        });
+        tx
+    });
+
+    let (tx, rx) = oneshot::channel();
+    SENDER.send((messages.clone(), tx)).await.unwrap();
+    rx.await.unwrap()
+}
+
 async fn chat(req: Request) -> Response {
     let Request::Post(body) = req else {
         return Err(http::StatusCode::METHOD_NOT_ALLOWED);
@@ -29,13 +50,8 @@ async fn chat(req: Request) -> Response {
 
     let messages = Arc::new(chat_req.messages);
 
-    let messages_clone = messages.clone();
-    let (generated, idx) = join!(
-        tokio::spawn(async move { chatbot::query_chat(&messages_clone).await }),
-        chatbot::gen_random_number()
-    );
+    let (generated, idx) = join!(query_chat(&messages), chatbot::gen_random_number());
 
-    let generated = generated.unwrap();
     let new_message = generated[idx % generated.len()].clone();
     let mut messages = Arc::into_inner(messages).unwrap();
     messages.push(new_message);
